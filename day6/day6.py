6f019cbb41b8b99ab765f18db60e053171a16259
stevendimaria <dimariasteve@gmail.com>
2025-12-06 09:43:11 -0500

    Add Day 6

--- day6/day6.py
+++ day6/day6.py
@@ -2,18 +2,15 @@ with open('input6.txt', 'r') as f:
     lines = f.read().splitlines()
     ops = [l for l in lines.pop().split()]
     probs = {i: [op] for i, op in enumerate(ops)}
-    str_probs = {i: [op] for i, op in enumerate(ops)}
     for line in lines:
         for i, l in enumerate(line.split()):
             probs[i].append(int(l))
-            str_probs[i].append(l)
 
 
 def calc(op, nums):
     if op == '+':
         return sum(nums)
-    else:
-        return (lambda p=1: [p := p * x for x in nums] and p)()
+    return (lambda p=1: [p := p * x for x in nums] and p)()
 
 
 def star1():
@@ -29,8 +26,7 @@ def star2():
     for line in lines:
         rows.append([x for x in line])
 
-    op = ops.pop()
-    nums, rem = [], len(rows[0])
+    op, nums, rem = ops.pop(), [], len(rows[0])
     while True:
         if rem:
             curr = [row.pop() for row in rows]
